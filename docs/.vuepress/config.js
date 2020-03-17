747b1a8f30cf2ac330b0747441d8662a93219497
liaozhen1 <2388189399@qq.com>
2020-03-17 23:05:15 +0800

    开始手写promise

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -44,9 +44,7 @@ module.exports = {
                 ['sixteen', 'Promise'],
                 ['eighteen', 'JS中获取元素宽高和位置各属性的区别'],
                 ['nineteen',  'async和await'],
-                ['twenty', 'JS数组方法总结'],
-                ['twentyOne', 'JS设计模式'],
-                ['twentyTwo', 'promise的设计与实现'],
+                ['twenty', 'JS数组方法总结']
             ]
         }
     },
